05064dd0045041d99336733e7b21dd90d07634cd
elisa <bascongarcia@gmail.com>
2022-09-03 20:49:53 +0200

    Model Bike Done

---
+++ models/Bike.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const bikeSchema = new Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  url: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  terrain: {
+    type: String,
+    required: true
+  },
+  biketype: {
+    type: String,
+    required: true
+  },
+  material: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  }
+  
+});
+
+module.exports = model("Bike", bikeSchema);
